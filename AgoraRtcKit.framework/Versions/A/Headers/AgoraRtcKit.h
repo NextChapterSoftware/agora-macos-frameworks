ed7f274d94a42cf342cfc6430a6abe8776bb88bb
Peter Werry <pwerry@gmail.com>
2022-04-08 10:34:59 -0700

    Add Agora 4.0.0 Preview 6 frameworks

---
+++ AgoraRtcKit.framework/Versions/A/Headers/AgoraRtcKit.h
@@ -0,0 +1,10 @@
+//
+//  AgoraRtcKit.h
+//  AgoraRtcKit
+//
+//  Copyright (c) 2018 Agora. All rights reserved.
+//
+
+#import <AgoraRtcKit/AgoraRtcEngineKit.h>
+#import <AgoraRtcKit/AgoraRtcEngineKitEx.h>
+#import <AgoraRtcKit/AgoraSpatialAudioKit.h>
